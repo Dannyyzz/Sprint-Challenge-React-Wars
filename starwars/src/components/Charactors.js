c1f815f99fd965c54b121b209e344e1fc920f853
Dannyyzz <35716281+Dannyyzz@users.noreply.github.com>
2019-06-09 20:01:07 -0400

    Added Charactor.js conponent

---
+++ starwars/src/components/Charactors.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './StarWars.css';
+
+const Charactors = props => {
+    return (
+        <div classname = "charactorCard">
+            Charactor Card
+        </div>
+    );
+};
+
+export default Charactors;
