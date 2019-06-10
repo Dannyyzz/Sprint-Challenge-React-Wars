5edad4497635ce1d8cb19ed4559adbb95c367cd5
Dannyyzz <35716281+Dannyyzz@users.noreply.github.com>
2019-06-09 20:03:48 -0400

    Added Charactorlist.js conponent

---
+++ starwars/src/components/Charactorlist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Charactorlist from './Charactors.js';
+
+import './StarWars.css';
+
+const Charactorlist = props => {
+    return (
+        <div classname = 'Charactorlist-Wrap'>
+             
+        </div>
+    );
+};
+
+export default Charactorlist;
